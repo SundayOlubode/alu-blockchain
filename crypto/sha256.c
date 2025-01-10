c2d23b5567079f88f7683d280fff644341b498b0
Sam <s.olubode@alustudent.com>
2025-01-10 10:04:37 +0100

    sha256

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -10,45 +10,12 @@
  */
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-        // init algorithm context
-        // update context with hash
-        // finalize hash computation
-
-        EVP_MD_CTX *context = NULL;
-        EVP_MD *algorithm = NULL;
-
-        if (digest == NULL)
+        if (!s || !digest)
         {
-                return (NULL);
+                return NULL;
         }
 
-        context = EVP_MD_CTX_new();
-        if (!context)
-        {
-                return (NULL);
-        }
-
-        algorithm = EVP_MD_fetch(NULL, "SHA256", NULL);
-        // algorithm = EVP_sha256();
-
-        if (!EVP_DigestInit_ex(context, algorithm, NULL))
-        {
-                printf("Error initializing hash function\n");
-        }
-
-        if (!EVP_DigestUpdate(context, s, len))
-        {
-                printf("Error updating hash\n");
-        }
-
-        if (!EVP_DigestFinal_ex(context, digest, &len))
-        {
-                printf("Error finalizing hash\n");
-        }
-
-        return (digest);
-
-        printf("Digest: %s\n", &digest);
+        return SHA256((const unsigned char *)s, len, digest);
 }
 
 // int main(void)
@@ -56,8 +23,9 @@ uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH
 //         uint8_t digest[SHA256_DIGEST_LENGTH];
 //         uint8_t *hash;
 //         size_t len = 9;
+//         int8_t const *message = "Holberton";
 
-//         hash = sha256("Holberton", len, digest);
+//         hash = sha256(message, len, digest);
 //         if (!hash)
 //         {
 //                 printf("sha256() failed\n");
