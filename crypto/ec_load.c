ba6e105104bcb84f4f3c6152521b443d8e78dfdc
Sam <s.olubode@alustudent.com>
2025-01-10 17:16:42 +0100

    ec_load.c

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -8,42 +8,42 @@
  */
 EC_KEY *ec_load(char const *folder)
 {
-        FILE *fp;
-        char path[256] = {0};
-        EC_KEY *key = NULL;
+	FILE *fp;
+	char path[256] = {0};
+	EC_KEY *key = NULL;
 
-        if (!folder)
-                return (0);
+	if (!folder)
+		return (0);
 
-        /*Create the complete path to read the public key*/
-        sprintf(path, "%s/" PUB_FILENAME, folder);
-        /*Open a file descriptor to read the public key*/
-        fp = fopen(path, "r");
-        if (!fp)
-        {
-                EC_KEY_free(key);
-                return (0);
-        }
-        /*Read the public key in the given file and store it in key*/
-        if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
-        {
-                EC_KEY_free(key);
-                fclose(fp);
-                return (0);
-        }
-        fclose(fp);
-        /*Create the complete path to read the private key*/
-        sprintf(path, "%s/" PRI_FILENAME, folder);
-        /*Open a file descriptor to read the private key*/
-        fp = fopen(path, "r");
-        if (!fp)
-                return (0);
-        /*read the private key in the given file and store it in key*/
-        if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
-        {
-                fclose(fp);
-                return (0);
-        }
-        fclose(fp);
-        return (key);
+	/*Create the complete path to read the public key*/
+	sprintf(path, "%s/" PUB_FILENAME, folder);
+	/*Open a file descriptor to read the public key*/
+	fp = fopen(path, "r");
+	if (!fp)
+	{
+		EC_KEY_free(key);
+		return (0);
+	}
+	/*Read the public key in the given file and store it in key*/
+	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+	{
+		EC_KEY_free(key);
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+	/*Create the complete path to read the private key*/
+	sprintf(path, "%s/" PRI_FILENAME, folder);
+	/*Open a file descriptor to read the private key*/
+	fp = fopen(path, "r");
+	if (!fp)
+		return (0);
+	/*read the private key in the given file and store it in key*/
+	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
+	{
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+	return (key);
 }
