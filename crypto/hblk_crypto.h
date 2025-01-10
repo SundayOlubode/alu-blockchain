65da5fbfb71d8188ee4699ee058a6f1b3f6a3f61
Sam <s.olubode@alustudent.com>
2025-01-10 12:48:05 +0100

    ec_to_pub.c

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -10,8 +10,11 @@
 #include <openssl/err.h>
 #include <openssl/ec.h>
 
+#define EC_PUB_LEN 65
+
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
+uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
 #endif /* HBLK_CRYPTO_H */
