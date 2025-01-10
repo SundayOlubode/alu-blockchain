45e9df1e05341dafcd23ea1eaba8628db132e710
Sam <s.olubode@alustudent.com>
2025-01-10 16:57:59 +0100

    ec_from_pub.c

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -17,9 +17,7 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 
 	key = EC_KEY_new_by_curve_name(EC_CURVE);
 	if (!key)
-	{
 		return (NULL);
-	}
 
 	group = EC_KEY_get0_group(key);
 	if (!group)
@@ -34,7 +32,6 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 		EC_KEY_free(key);
 		return (NULL);
 	}
-
 	if (!EC_POINT_oct2point(group, point, pub, EC_PUB_LEN, NULL))
 	{
 		EC_POINT_free(point);
@@ -48,7 +45,6 @@ EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 		EC_KEY_free(key);
 		return (NULL);
 	}
-
 	EC_POINT_free(point);
 	return (key);
 }
